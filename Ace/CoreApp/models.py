1ac6bcab6d5d48324c56126b5cd3b72133ab3ee4
Amol <includeamol@gmail.com>
2022-09-20 11:56:43 +0530

    FarmerInfo Model Added

--- Ace/CoreApp/models.py
+++ Ace/CoreApp/models.py
@@ -1,3 +1,17 @@
+from tkinter.tix import Balloon
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class FarmerInfo(models.Model):
+    first_name =    models.CharField(max_length=50)
+    last_name =     models.CharField(max_length=50, blank=True)
+    state_name =    models.CharField(max_length=50, blank=True)
+    district_name = models.CharField(max_length=50, blank=True)
+    village_name =  models.CharField(max_length=50, blank=True)
+    phone =         PhoneNumberField(unique=True, blank=True)
+
+    text_column_1 = models.TextField(blank=True)
+    text_column_2 = models.TextField(blank=True)
+    text_column_3 = models.TextField(blank=True)
+    text_column_4 = models.TextField(blank=True)
+    
