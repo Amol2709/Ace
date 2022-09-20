a9d5c63a506cd57d8824d374d89471e7ecc61865
Amol <includeamol@gmail.com>
2022-09-20 15:24:13 +0530

    Altered Model Field Behaviour

--- Ace/CoreApp/models.py
+++ Ace/CoreApp/models.py
@@ -4,14 +4,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class FarmerInfo(models.Model):
     first_name =    models.CharField(max_length=50)
-    last_name =     models.CharField(max_length=50, blank=True)
-    state_name =    models.CharField(max_length=50, blank=True)
-    district_name = models.CharField(max_length=50, blank=True)
-    village_name =  models.CharField(max_length=50, blank=True)
-    phone =         PhoneNumberField(unique=True, blank=True)
+    last_name =     models.CharField(max_length=50, null=True)
+    state_name =    models.CharField(max_length=50, null=True)
+    district_name = models.CharField(max_length=50, null=True)
+    village_name =  models.CharField(max_length=50, null=True)
+    phone =         PhoneNumberField(unique=True)
 
-    text_column_1 = models.TextField(blank=True)
-    text_column_2 = models.TextField(blank=True)
-    text_column_3 = models.TextField(blank=True)
-    text_column_4 = models.TextField(blank=True)
+    text_column_1 = models.TextField(null=True)
+    text_column_2 = models.TextField(null=True)
+    text_column_3 = models.TextField(null=True)
+    text_column_4 = models.TextField(null=True)
     
