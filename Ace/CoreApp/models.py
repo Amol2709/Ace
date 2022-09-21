a7780591010206488590e6ff5fa2b07ee091f7ce
Amol <includeamol@gmail.com>
2022-09-21 09:21:27 +0530

    Models Added for 4 different Languages

--- Ace/CoreApp/models.py
+++ Ace/CoreApp/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from tkinter.tix import Balloon
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
@@ -8,10 +9,70 @@ class FarmerInfo(models.Model):
     state_name =    models.CharField(max_length=50, null=True)
     district_name = models.CharField(max_length=50, null=True)
     village_name =  models.CharField(max_length=50, null=True)
-    phone =         PhoneNumberField()
+    phone =         PhoneNumberField(unique=True)
+
+    text_column_1 = models.TextField(null=True)
+    text_column_2 = models.TextField(null=True)
+    text_column_3 = models.TextField(null=True)
+    text_column_4 = models.TextField(null=True)
+
+
+class FarmerInfoHindi(models.Model):
+    farmer_id  = models.ForeignKey(FarmerInfo, on_delete=models.CASCADE)
+    first_name =    models.CharField(max_length=50)
+    last_name =     models.CharField(max_length=50, null=True)
+    state_name =    models.CharField(max_length=50, null=True)
+    district_name = models.CharField(max_length=50, null=True)
+    village_name =  models.CharField(max_length=50, null=True)
+    phone =         PhoneNumberField(unique=True)
+
+    text_column_1 = models.TextField(null=True)
+    text_column_2 = models.TextField(null=True)
+    text_column_3 = models.TextField(null=True)
+    text_column_4 = models.TextField(null=True)
+
+
+class FarmerInfoPunjabi(models.Model):
+    farmer_id  = models.ForeignKey(FarmerInfo, on_delete=models.CASCADE)
+    first_name =    models.CharField(max_length=50)
+    last_name =     models.CharField(max_length=50, null=True)
+    state_name =    models.CharField(max_length=50, null=True)
+    district_name = models.CharField(max_length=50, null=True)
+    village_name =  models.CharField(max_length=50, null=True)
+    phone =         PhoneNumberField(unique=True)
 
     text_column_1 = models.TextField(null=True)
     text_column_2 = models.TextField(null=True)
     text_column_3 = models.TextField(null=True)
     text_column_4 = models.TextField(null=True)
     
+
+class FarmerInfoMarathi(models.Model):
+    farmer_id  = models.ForeignKey(FarmerInfo, on_delete=models.CASCADE)
+    first_name =    models.CharField(max_length=50)
+    last_name =     models.CharField(max_length=50, null=True)
+    state_name =    models.CharField(max_length=50, null=True)
+    district_name = models.CharField(max_length=50, null=True)
+    village_name =  models.CharField(max_length=50, null=True)
+    phone =         PhoneNumberField(unique=True)
+
+    text_column_1 = models.TextField(null=True)
+    text_column_2 = models.TextField(null=True)
+    text_column_3 = models.TextField(null=True)
+    text_column_4 = models.TextField(null=True)
+
+
+
+class FarmerInfoTelugu(models.Model):
+    farmer_id  = models.ForeignKey(FarmerInfo, on_delete=models.CASCADE)
+    first_name =    models.CharField(max_length=50)
+    last_name =     models.CharField(max_length=50, null=True)
+    state_name =    models.CharField(max_length=50, null=True)
+    district_name = models.CharField(max_length=50, null=True)
+    village_name =  models.CharField(max_length=50, null=True)
+    phone =         PhoneNumberField(unique=True)
+
+    text_column_1 = models.TextField(null=True)
+    text_column_2 = models.TextField(null=True)
+    text_column_3 = models.TextField(null=True)
+    text_column_4 = models.TextField(null=True)
